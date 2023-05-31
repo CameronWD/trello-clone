1dde5efc67839d39b061587ee5e3eee8017ac64c
CameronWD <14041@coderacademy.edu.au>
2023-05-31 12:04:47 +1000

    added create command for table

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spameggs123@localhost:5432/trello'
+
+db = SQLAlchemy(app)
+# print(db.__dict__)
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.Text())
+    date_created = db.Column(db.Date())
+
+@app.cli.command('create')
+def create_db():
+    db.create_all()
+    print('Tables created successfully')
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+ 
