196e59ce14121483b9f5c67a60e4df5ea6963c25
CameronWD <14041@coderacademy.edu.au>
2023-06-26 17:31:10 +1000

    Refactored app.py and .flaskenv

---
+++ .flaskenv
@@ -0,0 +1,5 @@
+FLASK_DEBUG=1
+FLASK_RUN_PORT=5000
+FLASK_APP=app:create_app
+
+when using flask run, this will be used to pick up any environments including ports
--- app.py
+++ app.py
@@ -1,35 +1,39 @@
 from flask import Flask
 from os import environ
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from init import db, ma, bcrypt, jwt
 from blueprints.cli_bp import cli_bp
 from blueprints.auth_bp import auth_bp
 from blueprints.cards_bp import cards_bp
 
-load_dotenv() # will be removed when refactored
+# load_dotenv() # will be removed when refactored
+#factory object below. When an object is tasked with creating and then building an object before returning it.
+def create_app(): 
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    app.config['JWT_SECRET_KEY'] = environ.get('JWT_KEY')
+    app.config["SQLALCHEMY_DATABASE_URI"] = environ.get('DB_URI')
 
-app.config['JWT_SECRET_KEY'] = environ.get('JWT_KEY')
-app.config["SQLALCHEMY_DATABASE_URI"] = environ.get('DB_URI')
+    db.init_app(app)
+    ma.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
 
-db.init_app(app)
-ma.init_app(app)
-bcrypt.init_app(app)
-jwt.init_app(app)
+    @app.errorhandler(401)
+    def unauthorized(err):
+        return{'error': 'You must be an admin'}, 401
 
-@app.errorhandler(401)
-def unauthorized(err):
-    return{'error': 'You must be an admin'}, 401
+    app.register_blueprint(cli_bp)
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(cards_bp)
 
-app.register_blueprint(cli_bp)
+    return app
 
-app.register_blueprint(auth_bp)
 
-app.register_blueprint(cards_bp)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
 
 # db = SQLAlchemy(app)
